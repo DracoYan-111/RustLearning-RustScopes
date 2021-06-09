104579f105510fb0c1b2d01e11f188537e92e931
29449 <2944972991@qq.com>
2021-06-09 16:15:01 +0800

    移动的概念

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,19 @@
 fn main() {
     //s不可用
-    let s: &str = "hello";//s可用
-    //以下可以对s进行相关操作
+    let a: &str = "hello";//a可用
+    //以下可以对a进行相关操作
+
     //---------------------
-    let mut m = String::from("Hello");
-    //push_str:向m变量后加入某字符串
-    m.push_str(".World");
+    let mut b = String::from("Hello");
+    //push_str:向b变量后加入某字符串
+    b.push_str(".World");
+    println!("{}", b);//输出Hello.World
 
-    println!("{}", m);//输出Hello.World
-}//s作用域结束，s不再可用
+    //---------------------
+    //当c声明时，字符内容会被保存在heap(堆)
+    let c = String::from("Hello");
+    //当c1 = c时，c的内容会被移动
+    let c1 = c;
+    println!("{}", c);//此时c将不再可用
 
+}//a作用域结束，a不再可用
